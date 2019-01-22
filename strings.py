dbe391a9b79d81acbbfb09c8c9c946a88db08605
Mohamed Eid <medoeid50@gmail.com>
2019-01-22 04:36:28 +0200

    initial commit

---
+++ strings.py
@@ -0,0 +1,13 @@
+mystring = 'abcdefg'
+print(mystring)
+print(mystring[0])
+print(mystring[1:4])
+print(mystring[-1])
+print(mystring[0:])
+print(mystring[2:])
+print(mystring[:3])
+print(mystring[:2:])
+print(mystring.upper())
+print(mystring.capitalize())
+x = 'hello world'
+print(x.split())
